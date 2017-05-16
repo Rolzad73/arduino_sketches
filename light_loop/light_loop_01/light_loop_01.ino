09ffa6bcc421723d34b6e81bb4b9efde6de14137
Rolzad73 <rolzad73@gmail.com>
2017-05-15 22:32:20 -0400

    - added light_loop_01 and tone_keyboard

---
+++ light_loop/light_loop_01/light_loop_01.ino
@@ -0,0 +1,54 @@
+/*
+  Simple 8 LED cycle test
+ Ryan Wilson (c) 2017
+ */
+
+//  D0-D7:PORTD
+//  D8-D13:PORTB
+//  A0-A7:PORTC
+
+// #define pin_D1_on()  (PORTD |=  (1<<4))
+// #define pin_D1_off()  (PORTD &= ~(1<<4))
+
+
+void setup() {
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(0, HIGH);
+  delay(100);
+  digitalWrite(0, LOW);
+  digitalWrite(1, HIGH);
+  delay(100);
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  delay(100);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  delay(100);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  delay(100);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  delay(100);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);
+}
+
+
+
+
---
+++ tone_keyboard/tone_keyboard.ino
@@ -0,0 +1,58 @@
+// Simple 8 note organ (C major key)
+// created 15 May 2017
+// by Ryan Paul Wilson
+
+#include "pitches.h"
+
+int buttonState = 0;
+const int buttonStartPin = 2;
+const int buttonCount = 8;
+const int speakerPin = 12;
+
+int notesOctave1[] = {
+  NOTE_C4,
+  NOTE_D4,
+  NOTE_E4,
+  NOTE_F4,
+  NOTE_G4,
+  NOTE_A4,
+  NOTE_B4,
+  NOTE_C5
+};
+
+void setup() {
+  Serial.begin(9600);
+  // button pins
+  for (int input = buttonStartPin; input < buttonStartPin + buttonCount; input++) {
+    pinMode(input, INPUT_PULLUP);
+  }
+
+  // speaker pin
+  pinMode(speakerPin, OUTPUT);
+}
+
+void loop() {
+  boolean keyPressed = false;
+  for (int input = buttonStartPin; input < buttonStartPin + buttonCount; input++) {
+    // get a sensor reading
+    buttonState = digitalRead(input);
+
+    // play sound for key
+    if (buttonState == LOW && keyPressed == false) {
+      Serial.println(input); 
+      keyPressed = true;
+      tone(speakerPin, notesOctave1[input-buttonStartPin]);
+    }
+  }
+  
+  // silence speaker if no button pressed
+  if (!keyPressed) {
+      noTone(speakerPin);
+  }
+  
+  // stabalizing delay
+  delay(1);
+}
+
+
+
